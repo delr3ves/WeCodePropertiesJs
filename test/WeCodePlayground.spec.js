ec76cca69097de0a342d6ba62468a2873f58357d
Sergio Arroyo <sergio.arroyo.cuevas@gmail.com>
2019-02-20 01:01:57 +0100

    Define properites for sum

--- test/WeCodePlayground.spec.js
+++ test/WeCodePlayground.spec.js
@@ -1,5 +1,23 @@
+/*
+const sum = (a, b) => {
+  return a + b;
+};
+*/
+
 describe("WeCodeProperties", () => {
-  it("should be able to pass a dummy test", () => {
-    expect(true).to.equal(true);
+  it("keep the associative property", () => {
+    //TODO test the associative property
+  });
+
+  it("keep the commutative property", () => {
+    //TODO test the commutative property
+  });
+
+  it("have an identity value", () => {
+    //TODO test the identity property
+  });
+
+  it("actually sum a couple numbers", () => {
+    //TODO test the sum, maybe you can create a table to test a few cases...
   });
 });
