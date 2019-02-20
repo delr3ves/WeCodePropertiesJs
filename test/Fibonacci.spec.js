34b651df4261dadfc02eb103819c253c0be1e487
Sergio Arroyo <sergio.arroyo.cuevas@gmail.com>
2019-02-20 12:13:10 +0100

    Define fibonacci properties

---
+++ test/Fibonacci.spec.js
@@ -0,0 +1,19 @@
+/*
+const fibonacci = num => {
+  var a = 1,
+    b = 0,
+    temp;
+
+  while (num >= 0) {
+    temp = a;
+    a = a + b;
+    b = temp;
+    num--;
+  }
+  return b;
+};
+*/
+
+describe("Fibonacci of n", () => {
+  jsc.property("keep the most important property", () => true);
+});
