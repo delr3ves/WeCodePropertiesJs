607c4b54aa46dd358e667e2f5d87a6ce507bf71a
Sergio Arroyo <sergio.arroyo.cuevas@gmail.com>
2019-02-21 18:54:00 +0100

    Add implementation

--- src/maxibon.js
+++ src/maxibon.js
@@ -7,14 +7,36 @@ export class Developer {
 
 export const Pedro = new Developer("Pedro", 3);
 export const Fran = new Developer("Fran", 1);
+export const Toni = new Developer("Toni", 1);
 export const Davide = new Developer("Davide", 0);
 export const Sergio = new Developer("Sergio", 2);
 export const Jorge = new Developer("Jorge", 1);
 
 export class KarumiHQs {
-  constructor(chat = new ConsoleChat()) { } // eslint-disable-line
+  constructor(chat = new ConsoleChat()) {
+    this.chat = chat;
+    this.maxibonsLeft = 10;
+  }
 
-  openFridge(devs) { } // eslint-disable-line
+  openFridge(devs) {
+    if (Array.isArray(devs)) {
+      openFridge(devs, this);
+    } else {
+      openFridge([devs], this);
+    }
+  }
+}
+
+function openFridge(devs, office) {
+  devs.forEach(dev => {
+    office.maxibonsLeft = office.maxibonsLeft - dev.maxibonsToGrab;
+    if (office.maxibonsLeft <= 2) {
+      office.maxibonsLeft += 10;
+      office.chat.sendMessage(
+        `Hi guys, I'm ${dev.name}. We need more maxibons!`
+      );
+    }
+  });
 }
 
 class ConsoleChat {
